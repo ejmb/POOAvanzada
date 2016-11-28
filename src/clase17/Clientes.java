92c4bb6c607210e54c80af3c48eec4a3802e9825
emorel <emorel@INFORMATICA-143.local>
2016-11-28 13:23:40 -0300

    Avance Oficina

---
+++ src/clase17/Clientes.java
@@ -0,0 +1,154 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package clase17;
+
+import java.beans.PropertyChangeListener;
+import java.beans.PropertyChangeSupport;
+import java.io.Serializable;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+import javax.persistence.Transient;
+
+/**
+ *
+ * @author emorel
+ */
+@Entity
+@Table(name = "CLIENTES", catalog = "", schema = "UAA")
+@NamedQueries({
+    @NamedQuery(name = "Clientes.findAll", query = "SELECT c FROM Clientes c"),
+    @NamedQuery(name = "Clientes.findByCodigo", query = "SELECT c FROM Clientes c WHERE c.codigo = :codigo"),
+    @NamedQuery(name = "Clientes.findByNombre", query = "SELECT c FROM Clientes c WHERE c.nombre = :nombre"),
+    @NamedQuery(name = "Clientes.findByRuc", query = "SELECT c FROM Clientes c WHERE c.ruc = :ruc"),
+    @NamedQuery(name = "Clientes.findByDireccion", query = "SELECT c FROM Clientes c WHERE c.direccion = :direccion"),
+    @NamedQuery(name = "Clientes.findByDescuento", query = "SELECT c FROM Clientes c WHERE c.descuento = :descuento")})
+public class Clientes implements Serializable {
+
+    @Transient
+    private PropertyChangeSupport changeSupport = new PropertyChangeSupport(this);
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @Basic(optional = false)
+    @Column(name = "CODIGO")
+    private Integer codigo;
+    @Basic(optional = false)
+    @Column(name = "NOMBRE")
+    private String nombre;
+    @Basic(optional = false)
+    @Column(name = "RUC")
+    private String ruc;
+    @Basic(optional = false)
+    @Column(name = "DIRECCION")
+    private String direccion;
+    @Basic(optional = false)
+    @Column(name = "DESCUENTO")
+    private float descuento;
+
+    public Clientes() {
+    }
+
+    public Clientes(Integer codigo) {
+        this.codigo = codigo;
+    }
+
+    public Clientes(Integer codigo, String nombre, String ruc, String direccion, float descuento) {
+        this.codigo = codigo;
+        this.nombre = nombre;
+        this.ruc = ruc;
+        this.direccion = direccion;
+        this.descuento = descuento;
+    }
+
+    public Integer getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(Integer codigo) {
+        Integer oldCodigo = this.codigo;
+        this.codigo = codigo;
+        changeSupport.firePropertyChange("codigo", oldCodigo, codigo);
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        String oldNombre = this.nombre;
+        this.nombre = nombre;
+        changeSupport.firePropertyChange("nombre", oldNombre, nombre);
+    }
+
+    public String getRuc() {
+        return ruc;
+    }
+
+    public void setRuc(String ruc) {
+        String oldRuc = this.ruc;
+        this.ruc = ruc;
+        changeSupport.firePropertyChange("ruc", oldRuc, ruc);
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        String oldDireccion = this.direccion;
+        this.direccion = direccion;
+        changeSupport.firePropertyChange("direccion", oldDireccion, direccion);
+    }
+
+    public float getDescuento() {
+        return descuento;
+    }
+
+    public void setDescuento(float descuento) {
+        float oldDescuento = this.descuento;
+        this.descuento = descuento;
+        changeSupport.firePropertyChange("descuento", oldDescuento, descuento);
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (codigo != null ? codigo.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Clientes)) {
+            return false;
+        }
+        Clientes other = (Clientes) object;
+        if ((this.codigo == null && other.codigo != null) || (this.codigo != null && !this.codigo.equals(other.codigo))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "clase17.Clientes[ codigo=" + codigo + " ]";
+    }
+
+    public void addPropertyChangeListener(PropertyChangeListener listener) {
+        changeSupport.addPropertyChangeListener(listener);
+    }
+
+    public void removePropertyChangeListener(PropertyChangeListener listener) {
+        changeSupport.removePropertyChangeListener(listener);
+    }
+    
+}
